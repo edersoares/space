ab2473ced766f4edb8b40f06c1d0f2b579f7a12a
Eder Soares <edersoares@me.com>
2024-09-18 11:45:40 -0300

    Authorization

--- src/Http/Controllers/AuthorizationController.php
+++ src/Http/Controllers/AuthorizationController.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace Dex\Laravel\Space\Http\Controllers;
 
+use Dex\Laravel\Space\Models\User;
+use Illuminate\Http\Request;
+
 class AuthorizationController
 {
-    public function __invoke(): array
+    public function __invoke(Request $request)
     {
-        return [];
+        /** @var User $profile */
+        $profile = $request->user();
+
+        $profile->load('roles');
+
+        return $profile->getAllPermissions()->pluck('name');
     }
 }

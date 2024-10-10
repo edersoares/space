451ee078b2d329de9715b1bad61b3f397fb095cc
Eder Soares <edersoares@me.com>
2024-10-10 10:04:25 -0300

    Adds `account/me` endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Dex\Laravel\Space\Http\Controllers\MeController;
 use Dex\Laravel\Space\Http\Controllers\SpaceController;
 use Dex\Laravel\Space\Http\Controllers\UserController;
 use Dex\Laravel\Space\Http\Controllers\UserProfileController;
@@ -10,6 +11,7 @@ use Illuminate\Support\Facades\Route;
 use Orion\Facades\Orion;
 
 Route::group(['prefix' => 'api', 'middleware' => [AcceptJson::class, 'auth:api']], function () {
+    Route::get('me', MeController::class);
     Orion::resource('space', SpaceController::class);
     Orion::resource('user', UserController::class);
     Orion::hasManyResource('user', 'profile', UserProfileController::class);

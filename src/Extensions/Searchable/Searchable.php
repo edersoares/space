ebc7babfc5aa66d21232003beb5dff7aed3230eb
Eder Soares <edersoares@me.com>
2025-08-27 14:16:49 -0300

    Searchable

---
+++ src/Extensions/Searchable/Searchable.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Dex\Laravel\Space\Extensions\Searchable;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @mixin Model
+ */
+trait Searchable
+{
+    public function searchableBy(): array
+    {
+        return [
+            $this->getKeyName(),
+        ];
+    }
+
+    public function searchableKey(): string
+    {
+        return 'searchable';
+    }
+
+    protected function searchableTransform(Model $model): string
+    {
+        $attributes = $model->only($this->searchableBy());
+        $searchable = implode(' ', $attributes);
+
+        return str($searchable)->slug(' ')->value();
+    }
+
+    public static function bootSearchable(): void
+    {
+        static::saving(function (Model $model) {
+            /** @var Searchable $searchable */
+            $searchable = $model;
+
+            $model->{$searchable->searchableKey()} = $searchable->searchableTransform($model);
+        });
+    }
+}

76b93993eb991645eaadb4083b1be154e328d393
Eder Soares <edersoares@me.com>
2025-09-02 15:55:40 -0300

    Armazena a URL de redirecionamento

--- src/Extensions/Socialite/SocialRedirectController.php
+++ src/Extensions/Socialite/SocialRedirectController.php
@@ -5,12 +5,17 @@ declare(strict_types=1);
 namespace Dex\Laravel\Space\Extensions\Socialite;
 
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialRedirectController
 {
-    public function __invoke(string $provider): RedirectResponse
+    public function __invoke(Request $request, string $provider): RedirectResponse
     {
+        if ($request->has('intended')) {
+            $request->session()->put('url.intended', $request->get('intended'));
+        }
+
         return Socialite::driver($provider)->redirect();
     }
 }
